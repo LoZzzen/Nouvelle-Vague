8724ecb1a302f0965428035deb0364efa1cec4cb
Rokhiya <rokhiyatourebts@gmail.com>
2025-01-10 17:04:37 +0100

    test

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> VAGUE</title>
+    <title> NOUVELLE VAGUE</title>
     <style>
         /* Style global */
         body {
